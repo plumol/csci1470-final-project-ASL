67105a4a83b2521c5f204e6b5cba13706450717b
Grace Wan <grace_wan@brown.edu>
2023-04-23 12:29:00 -0400

    setting up model layers

---
+++ code/model.py
@@ -0,0 +1,26 @@
+import numpy as np
+import math
+import tensorflow as tf
+from keras.layers import \
+    Conv2D, MaxPool2D, Dropout, Flatten, Dense
+import sklearn
+
+class ASLClassifier(tf.keras.Model):
+    def __init__(self):
+        super(ASLClassifier, self).__init__()
+        self.num_epochs = 30
+        self.num_classes = 36
+        self.batch_size = 120
+        self.learning_rate = 0.01
+
+        self.layers = tf.keras.Sequential([
+            tf.keras.layers.Conv2D(32, (3,3)),
+            tf.keras.layers.Conv2D(64, (3,3)),
+            tf.keras.layers.MaxPool2D(strides=(2,2)),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dense(36)
+        ])
+
+
+    def call(self, x):
